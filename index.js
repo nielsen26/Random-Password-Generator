6fa601443725cd74a1a6dc26bed03700ce6a4958
Nielsen <nielsenangrio@gmail.com>
2022-07-08 16:14:42 -0700

    Finalizing

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const sym = ["~","`","!","@","#","$","%","^","&","*","(",")",
 "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
+const results = document.querySelectorAll(".result")
+
+let length = 15
+
 function generate_pass() {
-    
+    for (let n = 0; n < results.length; n++) {
+        let password = ""
+        for (let i = 0; i < length; i++) {
+            password += characters[Math.floor(Math.random() * characters.length)]
+        }
+        results[n].textContent = password
+    }
+}
+
+for (let i = 0; i < results.length; i++) {
+    results[i].addEventListener("click", function(){
+        const copyText = results[i]
+
+        /* Select the text field */
+        copyText.select()
+        copyText.setSelectionRange(0, 99999) /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.value)
+
+        alert("Copied the text: " + copyText.value)
+    })
 }
